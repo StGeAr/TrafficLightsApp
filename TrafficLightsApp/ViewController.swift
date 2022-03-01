146bf3307ae113811794899b06780fe4f7e7a6a8
German Stavitskiy <gera_stavickii@mail,.ru>
2022-03-01 23:50:14 +0300

    Displaying on screen

--- TrafficLightsApp/ViewController.swift
+++ TrafficLightsApp/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redView: UIView!
+    @IBOutlet var yellowView: UIView!
+    @IBOutlet var greenView: UIView!
+    @IBOutlet var switchLightButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redView.layer.cornerRadius = 70
+        yellowView.layer.cornerRadius = 70
+        greenView.layer.cornerRadius = 70
+        switchLightButton.layer.cornerRadius = 10
     }
 
-
+    @IBAction func switchLightButtonPressed() {
+        switchLightButton.setTitle("NEXT", for: .normal)
+        redView.alpha = 1
+        
+    }
+    
 }
 
